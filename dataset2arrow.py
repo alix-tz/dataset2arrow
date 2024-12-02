1afefabd65011ceed09689725bf1345f53caf0e8
Thibault Clérice <leponteineptique@gmail.com>
2024-12-02 15:40:47 +0100

    Adding sampling

--- dataset2arrow.py
+++ dataset2arrow.py
@@ -8,9 +8,11 @@ import pyarrow as pa
 import numpy as np
 import json
 import io
+import tabulate
 import click
 import re
 
+from numpy.f2py.cfuncs import callbacks
 
 logging.basicConfig()
 logger = logging.getLogger(__name__)
@@ -32,7 +34,7 @@ def build_arrow(dataset: DatasetDict, output_file: str = "filtered.arrow", recor
 
     :param dataset: A dataset loaded through load_dataset from one of our repository
     :param output_file: The target file to save
-    :recordbatch_size: Don't touch it if you don't know what it is. See Kraken doc.
+    :param recordbatch_size: Don't touch it if you don't know what it is. See Kraken doc.
 
     """
     logger.info('Assembling dataset alphabet.')
@@ -78,7 +80,7 @@ def build_arrow(dataset: DatasetDict, output_file: str = "filtered.arrow", recor
         elif target == "test":
             test_ind = np.ones(len(line_cache), dtype=bool)
 
-        logger.info(f"Split: {target}, Train: {tr_ind.sum()}, Validation: {val_ind.sum()}, Test: {test_ind.sum()}")
+        logger.debug(f"Split: {target}, Train: {tr_ind.sum()}, Validation: {val_ind.sum()}, Test: {test_ind.sum()}")
 
         train_mask = pa.array(tr_ind)
         val_mask = pa.array(val_ind)
@@ -98,7 +100,7 @@ def build_arrow(dataset: DatasetDict, output_file: str = "filtered.arrow", recor
                         row["im"].save(fp, format='png')
                         line_cache.append({"text": row["text"], "im": fp.getvalue()})  # Check format of row
                         if len(line_cache) == recordbatch_size:
-                            logger.info(f'Flushing {len(line_cache)} lines into {tmp_file}.')
+                            logger.debug(f'Flushing {len(line_cache)} lines into {tmp_file}.')
                             rbatch, counts = _make_record_batch(line_cache, target=split)
                             writer.write(rbatch)
                             line_cache = []
@@ -129,7 +131,7 @@ def build_arrow(dataset: DatasetDict, output_file: str = "filtered.arrow", recor
     return
 
 
-def _parse_filters(ctx, param, values: str) -> List[Tuple[str, str]]:
+def _parse_filters(ctx, param, values: Tuple[str, ...]) -> List[Tuple[str, str]]:
     out = []
     for value in values:
         if re.match(r'(\w+)=(\w+)', value):
@@ -138,18 +140,56 @@ def _parse_filters(ctx, param, values: str) -> List[Tuple[str, str]]:
             raise click.BadParameter('.....')
     return out
 
+
+def _parse_sample(ctx, param, values: Tuple[str, str]) -> Tuple[str, int]:
+    try:
+        return values[0], int(values[1])
+    except:
+        raise
+        raise click.BadParameter('.....')
+
+class Sampler:
+    def __init__(self, column: str, max_value: int):
+        self.column: str = column
+        self.max_value: int = max_value
+        self.counter: Counter = Counter()
+
+    def __call__(self, row) -> bool:
+        self.counter[row[self.column]] += 1
+        return self.counter[row[self.column]] <= self.max_value
+
+    def reset(self):
+        self.counter = Counter()
+
+    def pretty_print(self):
+        print(
+            tabulate.tabulate(
+                [
+                    [key, value, f"{100*(max(value-self.max_value, 0) / value):.1f}%"]
+                    for key, value in self.counter.items()
+                ],
+                headers=["Value", "Found", "Discarded"],
+                tablefmt="md"
+            )
+        )
+
+
 @click.command()
 @click.argument("dataset")
 @click.argument("output", type=click.Path(file_okay=True, dir_okay=False))
 @click.option("-m", "--max-height", type=int, default=120)
 @click.option("-f", "--filters", callback=_parse_filters, multiple=True, default=[])
 @click.option("-v", "--verbose", default=False, is_flag=True)
-def cli(dataset, output, max_height, filters, verbose):
+@click.option("-s", "--sample", type=(str, int),
+              help="`--sample shelfmark 1000` will ensure you don't have more "
+                   "than 1000 rows with the same manuscript value per split")
+def cli(dataset, output, max_height, filters, verbose, sample):
     """ Convert [DATASET] into [OUTPUT], a Kraken arrow file. You can use filters such as
     `python dataset2arrow.py CATMuS/medieval-sample latin-9th-century.arrow --filters language=Latin --filters century=9`
 
     Use this commandline code as a documentation for your own modification.
     Using `--max-height 120` is recommended if you are using the default Kraken specs, as it lowers the file size
+    Using `--sample shelfmark 100` does not use more than 100 lines from each selfmark. Samples are retrieved in order
     """
     if verbose:
         logger.setLevel(logging.INFO)
@@ -160,6 +200,15 @@ def cli(dataset, output, max_height, filters, verbose):
             value = int(value)
         d = d.filter(lambda row: row[key] == value, batched=False)
     d = d.map(lambda x: resize_image(x, max_height))
+
+    if sample:
+        sampler = Sampler(sample[0], sample[1])
+        for split in d:
+            d[split] = d[split].filter(sampler)
+            if verbose:
+                sampler.pretty_print()
+            sampler.reset()
+
     build_arrow(d, output_file=output)
 
 
